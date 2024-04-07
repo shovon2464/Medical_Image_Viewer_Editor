97d631f49a6c35195c634fc7f80e3f0a931d87de
Shovon Bhowmick <shovon2464@gmail.com>
2024-04-07 13:56:43 -0600

    user segmentation function made but not implemented

--- src/index.js
+++ src/index.js
@@ -27,11 +27,13 @@ import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
 import  controlPanelISO from "./controlPanelISO.html";
 import style from 'style-loader!css-loader?modules!./customstyles.css';
 import vtkPiecewiseGaussianWidget from '@kitware/vtk.js/Interaction/Widgets/PiecewiseGaussianWidget';
-
 // Force the loading of HttpDataAccessHelper to support gzip decompression
 import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
 
-//import vtkColorMaps from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps';
+
+
+
+
 
 const __BASE_PATH__ = "../"; // Define your base path here
 
@@ -64,219 +66,64 @@ function preventDefaults(e) {
 }
 // ----------------------------------------------------------------------------
 
-//Slicer functiom
+function createViewerSegmentation(rootContainer, fileContents, options) {
 
-function setupMultiSlice({ renderer2, renderWindow2, source: data }) {
-  //initializing slices 
-  const imageActorI = vtkImageSlice.newInstance();
-  const imageActorJ = vtkImageSlice.newInstance();
-  const imageActorK = vtkImageSlice.newInstance();
+  /*
+  const div1 = document.createElement("div");
+  rootContainer.appendChild(div1);
 
-  //adding actors to the slices
-  renderer2.addActor(imageActorK);
-  renderer2.addActor(imageActorJ);
-  renderer2.addActor(imageActorI);
-
-  //function to update color level
-  function updateColorLevel(e) {
-    const colorLevel = Number(
-      (e ? e.target : document.querySelector(".colorLevel")).value
-    );
-    //setting color level property
-    imageActorI.getProperty().setColorLevel(colorLevel);
-    imageActorJ.getProperty().setColorLevel(colorLevel);
-    imageActorK.getProperty().setColorLevel(colorLevel);
-    renderWindow2.render();
-  }
-   //function to update color window
-  function updateColorWindow(e) {
-    const colorLevel = Number(
-      (e ? e.target : document.querySelector(".colorWindow")).value
-    );
-    //setting color window property
-    imageActorI.getProperty().setColorWindow(colorLevel);
-    imageActorJ.getProperty().setColorWindow(colorLevel);
-    imageActorK.getProperty().setColorWindow(colorLevel);
-    renderWindow2.render();
-  }
-
-  //-------------------------------
-  //initializing mapper and setting input data, slice values and mappers
-  const imageMapperK = vtkImageMapper.newInstance();
-  imageMapperK.setInputData(data);
-  imageMapperK.setKSlice(30);
-  imageActorK.setMapper(imageMapperK);
-
-  const imageMapperJ = vtkImageMapper.newInstance();
-  imageMapperJ.setInputData(data);
-  imageMapperJ.setJSlice(30);
-  imageActorJ.setMapper(imageMapperJ);
-
-  const imageMapperI = vtkImageMapper.newInstance();
-  imageMapperI.setInputData(data);
-  imageMapperI.setISlice(30);
-  imageActorI.setMapper(imageMapperI); 
-
-  //if the range of slicees are changed in the control panel input 
-  //then the value of slices will be updated in the window also
-  document.querySelector(".sliceI").addEventListener("input", (e) => {
-    imageActorI.getMapper().setISlice(Number(e.target.value));
-    renderWindow2.render();
+  const background = [0, 0, 0];
+  const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+    background,
+    rootContainer: div1,
   });
 
-  document.querySelector(".sliceJ").addEventListener("input", (e) => {
-    imageActorJ.getMapper().setJSlice(Number(e.target.value));
-    renderWindow2.render();
+  const renderer = fullScreenRenderer.getRenderer();
+  const renderWindow = fullScreenRenderer.getRenderWindow();
+  renderWindow.getInteractor().setDesiredUpdateRate(15);
+
+  // Set the size and z-index of the fullScreenRenderer container
+  fullScreenRenderer.getContainer().style.width = "1420px";
+  fullScreenRenderer.getContainer().style.height = "780px";
+  fullScreenRenderer.getContainer().style.marginLeft = "20%";
+  fullScreenRenderer.getContainer().style.marginTop =  "0%";
+
+  // Resize the fullScreenRenderer1
+  fullScreenRenderer.resize();
+
+   // Load the .vti file using vtk.js
+  const vtiReader = vtkXMLImageDataReader.newInstance({ fetchGzip: true });
+  vtiReader.parseAsArrayBuffer(fileContents);
+  const source = vtiReader.getOutputData();
+
+  const thresholdMin = 100; // Minimum intensity threshold
+  const thresholdMax = 200; // Maximum intensity threshold
+  const threshold = vtkImageThreshold.newInstance({
+    lowerThreshold: thresholdMin,
+    upperThreshold: thresholdMax,
   });
 
-  document.querySelector(".sliceK").addEventListener("input", (e) => {
-    imageActorK.getMapper().setKSlice(Number(e.target.value));
-    renderWindow2.render();
+  threshold.setInputData(vtiReader.getOutputData());
+  // Apply marching cubes to extract contours
+  const marchingCubes = vtkImageMarchingCubes.newInstance({
+    contourValue: 0.0, // Adjust contour value as needed
   });
+  marchingCubes.setInputConnection(threshold.getOutputPort());
 
-  //get the data range
-  const dataRange = data.getPointData().getScalars().getRange();
-  const extent = data.getExtent();
-
-  //setting attribute for values from control panel input 
-  [".sliceI", ".sliceJ", ".sliceK"].forEach((selector, idx) => {
-    const el = document.querySelector(selector);
-    el.setAttribute("min", extent[idx * 2 + 0]);
-    el.setAttribute("max", extent[idx * 2 + 1]);
-    el.setAttribute("value", 30);
-  });
-   //setting attribute for values from control panel input
-  [".colorLevel", ".colorWindow"].forEach((selector) => {
-    document.querySelector(selector).setAttribute("max", dataRange[1]);
-    document.querySelector(selector).setAttribute("value", dataRange[1]);
-  });
-  document
-    .querySelector(".colorLevel")
-    .setAttribute("value", (dataRange[0] + dataRange[1]) / 2);
-  updateColorLevel();
-  updateColorWindow();
-  //if color level is changed then call updateColorLevel function
-  document
-    .querySelector(".colorLevel")
-    .addEventListener("input", updateColorLevel);
-  document
-    .querySelector(".colorWindow")
-    .addEventListener("input", updateColorWindow);
-
-  //making the slices global
-  global.imageActorI = imageActorI;
-  global.imageActorJ = imageActorJ;
-  global.imageActorK = imageActorK; 
-
- 
-  renderer2.resetCamera();
-  renderWindow2.render();
-}
-
-//end of setupmultislice
-
-
-// cropper function
-function setupCropper({renderer3,renderWindow3}){
-  //setting up control panel
-  function setupControlPanel(data, cropFilter) {
-    const axes = ['I', 'J', 'K'];
-    const minmax = ['min', 'max'];
-  
-    const extent = data.getExtent();
-    //setting the attributes with the change of inputs from the control panel
-    axes.forEach((ax, axi) => {
-      minmax.forEach((m, mi) => {
-        const el = document.querySelector(`.${ax}${m}`);
-        el.setAttribute('min', extent[axi * 2]);
-        el.setAttribute('max', extent[axi * 2 + 1]);
-        el.setAttribute('value', extent[axi * 2 + mi]);
-  
-        el.addEventListener('input', () => {
-          const planes = cropFilter.getCroppingPlanes().slice();
-          planes[axi * 2 + mi] = Number(el.value);
-          cropFilter.setCroppingPlanes(...planes);          
-          renderWindow3.render();
-        });
-      });
-    });
-  }
-  
-  // creating filter
-  const cropFilter = vtkImageCropFilter.newInstance();  
-  const reader = vtkHttpDataSetReader.newInstance({ fetchGzip: true }); 
-  //creating pipeline 
-  const actor = vtkVolume.newInstance();
   const mapper = vtkVolumeMapper.newInstance();
-  mapper.setSampleDistance(1.1);
+  const actor = vtkVolume.newInstance();
+
   actor.setMapper(mapper);
-  
-  // create color and opacity transfer functions
-  const ctfun = vtkColorTransferFunction.newInstance();
-  ctfun.addRGBPoint(0, 85 / 255.0, 0, 0);
-  ctfun.addRGBPoint(95, 1.0, 1.0, 1.0);
-  ctfun.addRGBPoint(225, 0.66, 0.66, 0.5);
-  ctfun.addRGBPoint(255, 0.3, 1.0, 0.5);
-  const ofun = vtkPiecewiseFunction.newInstance();
-  ofun.addPoint(0.0, 0.0);
-  ofun.addPoint(255.0, 1.0);
-  actor.getProperty().setRGBTransferFunction(0, ctfun);
-  actor.getProperty().setScalarOpacity(0, ofun);
-  actor.getProperty().setScalarOpacityUnitDistance(0, 3.0);
-  actor.getProperty().setInterpolationTypeToLinear();
-  actor.getProperty().setUseGradientOpacity(0, true);
-  actor.getProperty().setGradientOpacityMinimumValue(0, 2);
-  actor.getProperty().setGradientOpacityMinimumOpacity(0, 0.0);
-  actor.getProperty().setGradientOpacityMaximumValue(0, 20);
-  actor.getProperty().setGradientOpacityMaximumOpacity(0, 1.0);
-  actor.getProperty().setShade(true);
-  actor.getProperty().setAmbient(0.2);
-  actor.getProperty().setDiffuse(0.7);
-  actor.getProperty().setSpecular(0.3);
-  actor.getProperty().setSpecularPower(8.0);
-  
-  //reading the data
-  reader.setUrl(`https://kitware.github.io/vtk-js/data/volume/headsq.vti`).then(() => {
-    reader.loadData().then(() => {
-      renderer3.addVolume(actor);
-  
-      const data = reader.getOutputData();
+  mapper.setInputData(segmentedImage);
 
-      //set up crop planes
-      cropFilter.setCroppingPlanes(...data.getExtent());
+   // Add the segmented actor to the renderer
+   renderer.addActor(actor);
+   renderer.resetCamera();
+   renderWindow.render();
+   */
 
-      //calling setupControlPanel function
-      setupControlPanel(data, cropFilter);
-      
-      //set up interactor, camera and renderwindow
-      const interactor = renderWindow3.getInteractor();
-      interactor.setDesiredUpdateRate(15.0);
-      renderer3.resetCamera();
-      renderWindow3.render();
-      
-    });
-  });
-
-  //set up input connection
-  cropFilter.setInputConnection(reader.getOutputPort());
-  mapper.setInputConnection(cropFilter.getOutputPort());  
-
-  //making the variables global
-  global.source = reader;
-  global.mapper = mapper;
-  global.actor = actor;
-  global.ctfun = ctfun;
-  global.ofun = ofun;
-  global.renderer = renderer3;
-  global.renderWindow = renderWindow3;
-  global.cropFilter = cropFilter;
 }
 
-//Volume Contour function
-
-
-//end of cropper
-//-----------------------------------------------------------------------------
 
 function createViewerVolume(rootContainer, fileContents, options) {
   const div1 = document.createElement("div");
@@ -600,6 +447,9 @@ function createViewerRender(rootContainer, fileContents, options) {
   buttonContainer3.addEventListener("click", () => {
     loadVolume(rootContainer,options);
   })
+  buttonContainer4.addEventListener("click", () => {
+    loadSegmentation(rootContainer,options);
+  })
   buttonContainer5.addEventListener("click", () => {
     const views = renderWindow.getViews();
     views.forEach((view) => {
@@ -680,6 +530,22 @@ export function loadVolume(container, options) {
       const reader = new FileReader();
       reader.onload = function onLoad(e) {
         createViewerVolume(container, reader.result, options);
+      };
+      reader.readAsArrayBuffer(options.file);
+    } else {
+      console.error("Unkown file...");
+    }
+  } 
+}
+export function loadSegmentation(container, options) {
+  autoInit = false;
+  //emptyContainer(container);
+
+  if (options.file) {
+    if (options.ext === "vti") {
+      const reader = new FileReader();
+      reader.onload = function onLoad(e) {
+        createViewerSegmentation(container, reader.result, options);
       };
       reader.readAsArrayBuffer(options.file);
     } else {
